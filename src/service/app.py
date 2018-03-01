645e69e3b68618ed9e18fc715a00e60237516ec2
MX Chai <mingxuan@live.com.sg>
2018-03-01 21:37:29 +0800

    Add /users/authenticate

--- src/service/app.py
+++ src/service/app.py
@@ -2,13 +2,14 @@
 
 import json
 import os
+from uuid import uuid4
 
 from flask_sqlalchemy import SQLAlchemy
 from database import db
 from flask import Flask, request
 from flask_cors import CORS
 
-from models import User
+from models import User, Token, Entry
 
 ##########################################
 ## Set up Flask application and database
@@ -24,7 +25,8 @@ CORS(app)
 db.init_app(app)
 
 # Remember to update this list
-ENDPOINT_LIST = ['/', '/meta/heartbeat', '/meta/members', '/diary', '/diary/create', '/diary/delete', 'diary/permission']
+ENDPOINT_LIST = ['/', '/meta/heartbeat', '/meta/members', '/diary', 
+                '/diary/create', '/diary/delete', '/diary/permission']
 
 #############################
 ## Helper functions
@@ -52,15 +54,38 @@ def make_json_response(data, status=True, code=200):
     )
     return response
 
-def verify_user_registration(request):
+def make_json_false_response():
+    # Helper function just to return False, since it's not handled by 
+    # make_json_response, which requires an explicit Error message
+    response = app.response_class(
+        response=json.dumps({'status': False}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+def verify_user_registration():
     # Checks if registration fields are all filled in
     if len(request.form) < 4:
-        import pdb; pdb.set_trace()
         return False
     elif (request.form['username'] and request.form['password'] 
         and request.form['fullname'] and request.form['age']):
         return True
     return False
+
+def add_user_token():
+    # Creates a fresh UUID and stores it in the database
+    # If a token for a user_id already exists, return the existing token
+    user_id = User.query.filter_by(username=request.form['username']).first().id
+    user_token = Token.query.filter_by(user_id=user_id).first()
+    if user_token:
+        return user_token.token
+    else:
+        new_uuid = str(uuid4())
+        token = Token(token=new_uuid, user_id=user_id)
+        db.session.add(token)
+        db.session.commit()
+        return new_uuid
 
 #############################
 ## Admin Routes
@@ -90,10 +115,12 @@ def meta_members():
 @app.route("/users/register", methods=['POST'])
 def register_user():
     if request.method == 'POST':
-        if not verify_user_registration(request):
+        if not verify_user_registration():
             return make_json_response("Please fill in all the required information", False)
         try:
-            user = User(username=request.form['username'], fullname=request.form['fullname'], age=request.form['age'])
+            user = User(username=request.form['username'], 
+                        fullname=request.form['fullname'], 
+                        age=request.form['age'])
             user.set_password(request.form['password'])
             db.session.add(user)
             db.session.commit()
@@ -103,7 +130,14 @@ def register_user():
 
 @app.route("/users/authenticate", methods=['POST'])
 def auth_user():
-    pass
+    if not (request.form['username'] and request.form['password']):
+        return make_json_response("Please fill in all the required information", False)
+    auth_user = User.query.filter_by(username=request.form['username']).first()
+    if auth_user:
+        if auth_user.check_password(request.form['password']):
+            token = add_user_token()
+            return make_json_response(token)
+    return make_json_false_response()
 
 @app.route("/users/expire", methods=['POST'])
 def expire_token():
@@ -148,7 +182,7 @@ if __name__ == '__main__':
     os.chdir(dname)
 
     # Checks if the sqlite database already exists
-    # TODO: need to change this when we move back to MySQL
+    # TODO: need to change this when we move back to MySQL      
     if not os.path.isfile('/tmp/test.db'):
-      setup_database(app)
+        setup_database(app)
     app.run(debug=False, port=8080, host="0.0.0.0")
