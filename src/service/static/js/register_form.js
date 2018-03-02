c4a22667140f1c9c4236e39e93934e0f768b24a8
garbanzos <yiyant@gmail.com>
2018-03-02 20:24:38 +0800

    attempt to link api to register ui

--- src/service/static/js/register_form.js
+++ src/service/static/js/register_form.js
@@ -1,3 +1,28 @@
+function handleRegisterFormSubmit() {
+    $("#submit").on('click', function(event) {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var fullname = $('#fullname').val();
+        var age = $('#age').val();
+
+        $.ajax({
+          url: '/users/register',
+          type: "POST",
+          dataType: 'json',
+          data: JSON.stringify({data: {"username": username, "password": password, "fullname": fullname, "age": age}}),
+          success: function(result) {
+            console.log(result);
+          },
+          error: function(xhr, resp, text) {
+            console.log(xhr, resp, text);
+          }
+        });
+        // alert("test");
+        event.preventDefault();
+      }
+    );
+}
+
 $(document).ready(function() {
-  console.log("test")
+  handleRegisterFormSubmit();
 });
