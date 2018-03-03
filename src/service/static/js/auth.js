6cecdd562d668ed4ab90e8c8dffd669a76ef0498
MX Chai <mingxuan@live.com.sg>
2018-03-03 13:02:50 +0800

    Update auth.js skeleton

--- src/service/static/js/auth.js
+++ src/service/static/js/auth.js
@@ -10,35 +10,11 @@ function readToken() {
 }
 
 function checkAuthenticated() {
-    $("#submit").on('click', function(event) {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        var fullname = $('#fullname').val();
-        var age = $('#age').val();
-        $.ajax({
-          url: 'http://localhost:8080/users/register',
-          type: "POST",
-          dataType: 'json',
-          contentType: "application/json; charset=utf-8",
-          data: JSON.stringify({"username": username, "password": password, "fullname": fullname, "age": age}),
-          success: function(result) {
-            if(result["status"] == true) {
-              Materialize.toast("Your account has been created!", 4000);
-              window.location.href = "/diary";
-            } else {
-              Materialize.toast(result["error"], 4000);
-            }
-          },
-          error: function(xhr, resp, text) {
-            Materialize.toast("Something went horribly wrong!", 4000);
-          }
-        });
-        event.preventDefault();
-      }
-    );
+  // TODO: implement this
+  return ""
 }
 
 $(document).ready(function() {
   // Checks if user has a token set in localstorage.token
-  checkAuthenticated();
+  // checkAuthenticated();
 });
