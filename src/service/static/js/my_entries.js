ef3cf7036ad557b72c01b32188d0819f74cd4f82
William Tan <tanwcw@gmail.com>
2018-03-04 18:20:23 +0800

    Implement delete post

--- src/service/static/js/my_entries.js
+++ src/service/static/js/my_entries.js
@@ -4,6 +4,8 @@ var MY_ENTRIES_METHOD = {
           template = Handlebars.compile(templateSource),
           entryHTML = template(resJSON);
      $('#entries-container').html(entryHTML);
+     deleteEntry();
+
   },
   loadEntryData : function(){
     $.ajax({
@@ -18,8 +20,30 @@ var MY_ENTRIES_METHOD = {
 };
 
 function deleteEntry(){
-  $("#delete-entry").on('click', function(event) {
+  $(".delete-entry").on("click", function(event) {
+  	var token = readToken();
+  	var id = $(".delete-entry").val();
+	  $.ajax({
+	    url: 'http://localhost:8080/diary/delete',
+	    type: "POST",
+	    dataType: 'json',
+	    contentType: "application/json; charset=utf-8",
+	    data: JSON.stringify({"token": token, "id": id}),
+	    success: function(response) {
+	      if(response["status"] == true) {
+	        Materialize.toast("Deleted Entry", 4000);
+	        window.location.href = "/diary/my_entries"
+	      } else {
+	        Materialize.toast("Authentication failed", 4000);
+	      }
+	    },
+	    error: function(xhr, resp, text) {
+	      Materialize.toast("Something went horribly wrong!", 4000);
+	    }
+	  });
+	  event.preventDefault();
   }
+  );
 }
 
 $(document).ready(function(){
