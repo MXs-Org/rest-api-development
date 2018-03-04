8f4da0322d91231889d74d422235a117022e71b5
William Tan <tanwcw@gmail.com>
2018-03-04 19:22:36 +0800

    Fix bug in delete and implement change in visibility

--- src/service/static/js/my_entries.js
+++ src/service/static/js/my_entries.js
@@ -4,8 +4,6 @@ var MY_ENTRIES_METHOD = {
           template = Handlebars.compile(templateSource),
           entryHTML = template(resJSON);
      $('#entries-container').html(entryHTML);
-     deleteEntry();
-
   },
   loadEntryData : function(){
     $.ajax({
@@ -19,31 +17,54 @@ var MY_ENTRIES_METHOD = {
   }
 };
 
-function deleteEntry(){
-  $(".delete-entry").on("click", function(event) {
-  	var token = readToken();
-  	var id = $(".delete-entry").val();
-	  $.ajax({
-	    url: 'http://localhost:8080/diary/delete',
-	    type: "POST",
-	    dataType: 'json',
-	    contentType: "application/json; charset=utf-8",
-	    data: JSON.stringify({"token": token, "id": id}),
-	    success: function(response) {
-	      if(response["status"] == true) {
-	        Materialize.toast("Deleted Entry", 4000);
-	        window.location.href = "/diary/my_entries"
-	      } else {
-	        Materialize.toast("Authentication failed", 4000);
-	      }
-	    },
-	    error: function(xhr, resp, text) {
-	      Materialize.toast("Something went horribly wrong!", 4000);
-	    }
-	  });
-	  event.preventDefault();
-  }
-  );
+function deleteEntry(entryId){
+  var token = readToken();
+  var id = entryId;
+  $.ajax({
+    url: 'http://localhost:8080/diary/delete',
+    type: "POST",
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify({"token": token, "id": id}),
+    success: function(response) {
+      if(response["status"] == true) {
+        Materialize.toast("Deleted Entry", 4000);
+        window.location.href = "/diary/my_entries"
+      } else {
+        Materialize.toast("Authentication failed", 4000);
+      }
+    },
+    error: function(xhr, resp, text) {
+      Materialize.toast("Something went horribly wrong!", 4000);
+    }
+  });
+  event.preventDefault();
+}
+
+function changePermissionEntry(entryId, entryPublic){
+  var token = readToken();
+  var id = entryId;
+  var public = entryPublic;
+  public = !public;	
+  $.ajax({
+    url: 'http://localhost:8080/diary/permission',
+    type: "POST",
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify({"token": token, "id": id, "public": public}),
+    success: function(response) {
+      if(response["status"] == true) {
+        Materialize.toast("Changed entry visibility", 4000);
+        window.location.href = "/diary/my_entries"
+      } else {
+        Materialize.toast("Authentication failed", 4000);
+      }
+    },
+    error: function(xhr, resp, text) {
+      Materialize.toast("Something went horribly wrong!", 4000);
+    }
+  });
+  event.preventDefault();
 }
 
 $(document).ready(function(){
