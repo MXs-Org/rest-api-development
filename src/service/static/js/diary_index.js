800566c5eb027cadb0d66f1be60614d1ba2a2165
William Tan <tanwcw@gmail.com>
2018-03-05 14:00:58 +0800

    Default message when no public entries

--- src/service/static/js/diary_index.js
+++ src/service/static/js/diary_index.js
@@ -1,11 +1,14 @@
 var DIARY_METHOD = {
   handlerData:function(resJSON){
-    console.log(resJSON);
-    console.log($("#entry-template").html());
-      var templateSource   = $("#entry-template").html(),
-          template = Handlebars.compile(templateSource),
-          entryHTML = template(resJSON);
-     $('#entries-container').html(entryHTML);
+    var templateSource   = $("#entry-template").html(),
+        template = Handlebars.compile(templateSource),
+        entryHTML = template(resJSON);
+    header = "<div class='col s12'><h5>Public Posts</h5>"
+    if(entryHTML.trim() == ""){
+      $('#entries-container').html(header + "<div class='card-panel'><div class='row'>No public posts! Why don't you create one?</div></div></div>")
+    } else {
+      $('#entries-container').html(header + entryHTML);
+    }
   },
   loadEntryData : function(){
     $.ajax({
